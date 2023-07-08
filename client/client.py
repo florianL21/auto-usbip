a60153c26a5c905f79e8e24bd29061544302615e
Florian Laschober <flo.laschober@gmail.com>
2023-07-08 14:20:13 +0200

    Added server connection info

--- client/client.py
+++ client/client.py
@@ -14,37 +14,21 @@ import logging
 import sys
 from serial.tools import list_ports
 from serial.tools.list_ports_common import ListPortInfo
+from urllib3 import Retry
 
-PORT = 3240
 USB_ID_REGEX = re.compile(r"^\s+([A-Za-z0-9.\-_]+)\:.*$")
+
+# Port to use for pinging usbip servers
+PORT = 3240
+
+# Seconds to wait between polling the server for available connections
 POLLING_TIME = 2
+
+# How long to keep a connected port alive after the server has gone offline
 PORT_KEEPALIVE_TIMEOUT = 5
 
-
-def ping_server(server: str, port: int, timeout=3):
-    try:
-        socket.setdefaulttimeout(timeout)
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((server, port))
-    except OSError as error:
-        return False
-    else:
-        s.close()
-        return True
-
-
-def get_remote_usb_devices(ip: str):
-    p = subprocess.run(["usbip", "list", "-p", "-r", ip], capture_output=True)
-    usbids: list[str] = []
-    if p.returncode != 0:
-        logger.error(f"Error getting list of remote usb devices: {p.stderr.decode()}")
-        return usbids
-    output = p.stdout.decode()
-    for line in output.split("\n"):
-        match = USB_ID_REGEX.match(line)
-        if match:
-            usbids.append(match.group(1))
-    return usbids
+# Seconds since last ping after a server connection is rechecked
+SERVER_PING_CHECK = 10
 
 
 class ImportedDevice:
@@ -93,6 +77,46 @@ class ImportedDevice:
             return f"Port {self.port}: {self.kind} -> {self.desc}"
 
 
+class ServerConnection:
+    def __init__(self, ip: str, port: int) -> None:
+        self.ip = ip
+        self.port = port
+        self._ping_result = (time.time(), self._ping())
+
+    def _ping(self, timeout=3):
+        try:
+            socket.setdefaulttimeout(timeout)
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((self.ip, self.port))
+        except OSError:
+            return False
+        else:
+            s.close()
+            return True
+
+    @property
+    def is_alive(self):
+        last_time, _ = self._ping_result
+        current_time = time.time()
+        if time.time() - last_time > SERVER_PING_CHECK:
+            self._ping_result = (current_time, self._ping())
+        return self._ping_result[1]
+
+
+def get_remote_usb_devices(ip: str):
+    p = subprocess.run(["usbip", "list", "-p", "-r", ip], capture_output=True)
+    usbids: list[str] = []
+    if p.returncode != 0:
+        logger.error(f"Error getting list of remote usb devices: {p.stderr.decode()}")
+        return usbids
+    output = p.stdout.decode()
+    for line in output.split("\n"):
+        match = USB_ID_REGEX.match(line)
+        if match:
+            usbids.append(match.group(1))
+    return usbids
+
+
 def get_imported_devices():
     p = subprocess.run(["usbip", "port"], capture_output=True)
     ports: list[ImportedDevice] = []
@@ -130,9 +154,10 @@ def detach_all_ports():
 
 
 def main(
-    servers: list[str],
+    servers: list[ServerConnection],
     kill_signal: PipeConnection,
     try_icon: pystray.Icon,
+    server_list_lock: threading.Lock,
 ):
     os.environ["KEEPALIVE_TIMEOUT"] = str(PORT_KEEPALIVE_TIMEOUT)
     # detach all preexisting devices to start clean
@@ -141,11 +166,13 @@ def main(
     try:
         while kill_signal.poll() == False:
             available_devices: list[tuple[str, str]] = []
-            for server_ip in servers:
-                if ping_server(server_ip, PORT):
-                    available_devices += [
-                        (server_ip, id) for id in get_remote_usb_devices(server_ip)
-                    ]
+            if server_list_lock.acquire():
+                for server in servers:
+                    if server.is_alive:
+                        available_devices += [
+                            (server.ip, id) for id in get_remote_usb_devices(server.ip)
+                        ]
+                server_list_lock.release()
             for server, available_device in available_devices:
                 attach_device(server, available_device)
             try_icon.update_menu()
@@ -157,13 +184,29 @@ def main(
 def build_menu(
     icon: pystray.Icon,
     kill_signal: PipeConnection,
+    servers: list[ServerConnection],
+    server_list_lock: threading.Lock,
 ):
     def stop():
         icon.stop()
         kill_signal.send(True)
 
+    yield pystray.MenuItem("Servers:", action=lambda: None)
+    yield pystray.Menu.SEPARATOR
+    if server_list_lock.acquire(timeout=2):
+        for server in servers:
+            if server.is_alive:
+                yield pystray.MenuItem(f"[ONLINE] {server.ip}", action=lambda: None)
+            else:
+                yield pystray.MenuItem(
+                    f"[OFFLINE] {server.ip}", action=lambda: None, enabled=False
+                )
+        server_list_lock.release()
+    yield pystray.Menu.SEPARATOR
+    yield pystray.MenuItem("Attached devices:", action=lambda: None)
+    yield pystray.Menu.SEPARATOR
     for device in get_imported_devices():
-        yield pystray.MenuItem(str(device), action=lambda: None, enabled=False)
+        yield pystray.MenuItem(str(device), action=lambda: None)
     yield pystray.Menu.SEPARATOR
     yield pystray.MenuItem("Exit", action=stop)
 
@@ -176,7 +219,8 @@ if __name__ == "__main__":
         logger.error(
             "Please provide a list of server IP addresses as command line arguments"
         )
-    servers = sys.argv[1:]
+    servers = [ServerConnection(ip, PORT) for ip in sys.argv[1:]]
+    server_list_lock = threading.Lock()
     logo = Path("systray-logo.png")
     image = Image.open(logo)
     attached_devices: list[str] = []
@@ -186,7 +230,11 @@ if __name__ == "__main__":
         "auto-usbip",
         icon=image,
         title="Auto USB IP",
-        menu=pystray.Menu(lambda: build_menu(tray_icon, kill_signal_tx)),
+        menu=pystray.Menu(
+            lambda: build_menu(tray_icon, kill_signal_tx, servers, server_list_lock)
+        ),
     )
-    threading.Thread(target=lambda: main(servers, kill_signal_rx, tray_icon)).start()
+    threading.Thread(
+        target=lambda: main(servers, kill_signal_rx, tray_icon, server_list_lock)
+    ).start()
     tray_icon.run()
